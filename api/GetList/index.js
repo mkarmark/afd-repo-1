0dc31be8c3ac558adb55751f09293e62c05843fa
mkarmark <mikarmar@microsoft.com>
2021-11-05 13:44:33 -0700

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        headers: { 'foobar': 'baz'},
+        headers: { 'foobar': 'baz', 'Cache-Control': 'private, max-age=25'},
         body: req.headers
     }
 };
